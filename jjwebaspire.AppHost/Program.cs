0c5833e7684850aa161e47cc3392594ba6896856
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2024-07-11 15:12:46 +0200

    web a api sample project

--- jjwebaspire.AppHost/Program.cs
+++ jjwebaspire.AppHost/Program.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var apiService = builder.AddProject<Projects.jjwebaspire_api>("jjapi");
+
+builder.AddProject<Projects.jjwebaspire_web>("jjweb")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
 builder.Build().Run();
---
+++ jjwebaspire.Web/Program.cs
@@ -0,0 +1,38 @@
+using jjwebaspire.web.Pages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddHttpClient<WeatherApiClient>(client =>
+{
+    // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+    // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+    client.BaseAddress = new("https+http://jjapi");
+});
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
